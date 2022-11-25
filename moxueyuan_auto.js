2115fe101d2507cf703d92051072d7c612030e38
XiaoWeicheng <419949264@qq.com>
2022-11-26 00:42:59 +0800

    Update moxueyuan_auto.js

--- moxueyuan_auto.js
+++ moxueyuan_auto.js
@@ -1,29 +1,34 @@
 // ==UserScript==
-// @name         魔学院自动点击
+// @name         自动点击我在
 // @namespace    http://bestmind.space
 // @version      0.1
-// @description  魔学院自动点击
+// @description  自动点击我在
 // @author       xiaoweicheng
-// @match        https://*.moxueyuan.com/new/course/*
+// @downloadURL  https://github.com/XiaoWeicheng/tampermonkey_script/raw/main/moxueyuan_auto.js
+// @updateURL    https://github.com/XiaoWeicheng/tampermonkey_script/raw/main/moxueyuan_auto.js
+// @match        https://wanweitech.study.moxueyuan.com/new/course/*
 // @icon         https://static.moxueyuan.com/cdn/static/common/images/ht_gongkaike_icon.png
 // @grant        none
 // ==/UserScript==
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
+const sleep = async function(ms,func) {
+    await new Promise(resolve => setTimeout(resolve, ms))
+    func()
 }
 
-async function observe(divSelector, buttonSelector, log) {
-    while (true) {
-        const div = document.querySelector(divSelector);
-        if (div) {
-            new MutationObserver(function (mutations, observer) {
-                console.log(mutations)
-                mutations.forEach(function (mutation) {
-                    console.log(mutation)
-                    if (mutation.oldValue.indexOf("display: none") >= 0) {
+const observe = async function(divSelector, buttonSelector, log) {
+    while(true){
+        var div = document.querySelector(divSelector)
+        if(div){
+            var outObserver = new MutationObserver(function (mutations, observer) {
+                mutations.forEach(async function(mutation) {
+                    if(mutation.oldValue.indexOf("display: none")){
+                        observer.disconnect()
                         console.log(log)
                         document.querySelector(buttonSelector).click()
+                        sleep(60000,function(){
+                            observe(divSelector, buttonSelector, log)
+                        })
                     }
                 })
             }).observe(div, {
@@ -37,7 +42,7 @@ async function observe(divSelector, buttonSelector, log) {
     }
 }
 
-(function () {
+(function() {
 
     'use strict'
 
