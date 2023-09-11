afb4068277f9aa908b01a071e42a724b5897a13a
BestMind <26629886+XiaoWeicheng@users.noreply.github.com>
2023-09-11 16:32:42 +0800

    Update xiaoetong_tools.js

--- xiaoetong_tools.js
+++ xiaoetong_tools.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         小鹅通工具
 // @namespace    http://bestmind.space
-// @version      1.9
+// @version      2.0
 // @description  小鹅通工具
 // @author       xiaoweicheng
 // @downloadURL  https://github.com/XiaoWeicheng/tampermonkey_script/raw/main/xiaoetong_tools.js
